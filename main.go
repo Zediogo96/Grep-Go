336bdf444a573c5c32966187205e5079e006616e
Zediogo96 <josediogo96@outlook.com>
2024-03-05 20:51:17 +0000

    Extracted code to fetch files to a function and started and base for concurrency

--- main.go
+++ main.go
@@ -3,25 +3,44 @@ package main
 import (
 	"fmt"
 	"os"
+	"grep/worklist"
 )
 
-func main() {
-	// wl := worklist.New(100)
 
-	// numWorkers := 10
-
-	path := os.Args[2]
+func extractAllFiles(wl *worklist.Worklist, path string) {
 
 	// Get all the files in the current directory
 	entries, err := os.ReadDir(path)
 
+	// An error occurred with os.ReadDir
 	if err != nil {
 		fmt.Println("Error reading directory ", path)
 		return
 	}
 
+
+	// Iterate through all the files in the current directory
 	for _, entry := range entries {
-		fmt.Println(entry.Name())
+		if entry.IsDir() {
+			fmt.Println("Adding directory ", entry.Name())
+			nextPath := path + "/" + entry.Name()
+			extractAllFiles(wl, nextPath)
+
+		// Meaning that the entry is a file
+		} else {
+			fmt.Println("Adding file ", entry.Name())
+			wl.Add(worklist.NewJob(path + "/" + entry.Name()))
+		}
 	}
+}
+
+func main() {
+	wl := worklist.New(100)
+	numWorkers := 10
+
+	path := os.Args[2]
+
+	extractAllFiles(&wl, path)
+	wl.Finalize(numWorkers)
 
 }
